cd1404b9f27acd407d9f3dd188fb6853b4757830
abmetheny <abmetheny@gmail.com>
2023-05-05 10:02:39 -0400

    Added rules to webpack config file and added asset cacheing logic

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,6 +1,6 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
+const { CacheFirst, NetworkOnly } = require('workbox-strategies');
+const { registerRoute, setDefaultHandler } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
@@ -27,4 +27,49 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
+const pageFallback = 'offline.html';
+const imageFallback = false;
+const fontFallback = false;
+
+setDefaultHandler(new NetworkOnly());
+
+self.addEventListener('install', event => {
+  const files = [pageFallback];
+  if (imageFallback) {
+    files.push(imageFallback);
+  }
+  if (fontFallback) {
+    files.push(fontFallback);
+  }
+
+  event.waitUntil(
+    self.caches
+      .open('workbox-offline-fallbacks')
+      .then(cache => cache.addAll(files))
+  );
+});
+
+const handler = async options => {
+  const dest = options.request.destination;
+  const cache = await self.caches.open('workbox-offline-fallbacks');
+
+  if (dest === 'document') {
+    return (await cache.match(pageFallback)) || Response.error();
+  }
+
+  if (dest === 'image' && imageFallback !== false) {
+    return (await cache.match(imageFallback)) || Response.error();
+  }
+
+  if (dest === 'font' && fontFallback !== false) {
+    return (await cache.match(fontFallback)) || Response.error();
+  }
+
+  return Response.error();
+};
+
+setCatchHandler(handler);
+
+offlineFallback();
+
 registerRoute();
